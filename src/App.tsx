ab973aa7e035180a6cd41abd0b26eca2d56f99f7
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-01-30 22:07:21 +0200

    testing auto

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,60 @@
 import { useEffect, useState } from "react";
-import { randomNames } from "./randomNames";
-
+import { Employee, employees } from "./data";
+import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
+import { TextField } from "@mui/material";
 export function App() {
-  const [employees, setEmployees] = useState<{ email: string, name: string }[]>([]);
+  const [data,setData] = useState<Employee[]>([]);
+  const [filteredData, setFilteredData] = useState<Employee[]>([]);
+  const [query, setQuery] = useState<string | null>(null);
 
-  const [employeeForms, setEmployeeForms] = useState<JSX.Element[]>([]);
-
-  const names = randomNames
-  
-  // mock employees
   useEffect(() => {
-    const array = [];
-    for (let i = 0; i < 100; i++) {
-      const name = names[i % names.length];
-      array.push({ email: `${name}@gmail.com`, name: name });
+    setData(employees.slice(0,100));
+  }, data);
+
+  useEffect(() => {
+    setFilteredData(data);
+  }, data);
+
+  useEffect(() => {
+    if (!query) {
+      setFilteredData(data);
+      return;
     }
-    setEmployees(array);
-  }, []);
 
-  useEffect(() => {
-    setEmployeeForms(
-      employees.map((e) => (
-        <div>
-          <EmployeeForm key={e.email} employee={e} modifiedEmployee={{ isModified: false, email: '', name: '' }} />
-        </div>
-      ))
+    setFilteredData(() =>
+      data.filter((emp) => emp.userName.toLocaleLowerCase().indexOf(query) > -1)
     );
-  }, [employees]);
+  }, [query]);
 
-  function getModifiedEmployees() {
-    const modifiedEmployees = employeeForms.filter(e => e.props.children.props.modifiedEmployee.isModified).map((e) => e.props.children.props.modifiedEmployee);
-    console.log(modifiedEmployees);
-  }
+  const employeeElements = filteredData.map((emp) => (
+    <EmployeeRow key={emp.accountId} email={emp.userName}></EmployeeRow>
+  ));
 
   return (
-    <div className="App">
-      <h1>Hello React.</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button type="button" onClick={getModifiedEmployees}>
-        Save
-      </button>
-      {employeeForms}
+    <div>
+      <div>
+        <input
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div>{employeeElements}</div>
     </div>
   );
 }
 
-function EmployeeForm({ employee, modifiedEmployee }: { employee: { email: string, name: string }, modifiedEmployee: { isModified: boolean, email: string, name: string } }) {
-  const initialEmail = employee.email;
-  const initialName = employee.name;
-
-  const [email, setEmail] = useState(employee.email);
-  const [name, setName] = useState(employee.name);
-
-  useEffect(() => {
-    updateModifiedEmployee();
-  }, [name, email]);
-
-
-  useEffect(() => {
-    updateModifiedEmployee();
-  }, [])
-
+function EmployeeRow({ email }: { email: string }): JSX.Element {
+  console.log("rendering employee row");
   return (
-    <>
-      <input value={email} onChange={(event) => setEmail(event.target.value)} />
-      <input value={name} onChange={(event) => setName(event.target.value)} />
-    </>
+    <div>
+      <div>{email}</div>
+      <Autocomplete
+        options={[1, 2, 3, 4, 5, 6, 7]}
+        getOptionLabel={(option: number) => option.toString()}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Employee" />}
+      />
+    </div>
   );
-
-  function updateModifiedEmployee() {
-
-    modifiedEmployee.email = email;
-    modifiedEmployee.name = name;
-
-    if (initialEmail !== email || initialName !== name) {
-      modifiedEmployee.isModified = true;
-    } else {
-      modifiedEmployee.isModified = false;
-    }
-  }
 }
